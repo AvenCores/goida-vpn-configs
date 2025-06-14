04f9941a4cbdee92a7fdc38352dafbaba322d7e4
avencores <64781822+AvenCores@users.noreply.github.com>
2025-06-14 16:36:43 +0300

    fix

--- source/main.py
+++ source/main.py
@@ -29,7 +29,7 @@ URL3 = "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v
 REMOTE_FILE_PATH3 = "githubmirror/3.txt"
 LOCAL_FILE_PATH3 = "githubmirror/3.txt"
 
-URL4 = "https://xray.abvpn.ru/vless/9835fbea-cfb1-402a-bdc6-16ca140c6788/1100118643.json#abvpn"
+URL4 = "https://xray.abvpn.ru/vless/b4f3c864-3241-4abe-884e-adffcbbc5ffa/604720025.json#abvpn"
 REMOTE_FILE_PATH4 = "githubmirror/4.txt"
 LOCAL_FILE_PATH4 = "githubmirror/4.txt"
 
