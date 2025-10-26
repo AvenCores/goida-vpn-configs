71ef0ec54a71f2a3d41dee8b098f0b23132845c2
avencores <64781822+AvenCores@users.noreply.github.com>
2025-10-26 18:46:02 +0300

    update

--- source/main.py
+++ source/main.py
@@ -204,7 +204,7 @@ def update_readme_table():
         new_table = table_header + "\n" + "\n".join(table_rows)
 
         # Заменяем таблицу в README.md
-        table_pattern = r"\| № \| Файл \| Источник \| Последнее обновление \|[\s\S]*?\|--\|--\|--\|--\|[\s\S]*?(\n## |$)"
+        table_pattern = r"\| № \| Файл \| Источник \| Время \| Дата \|[\s\S]*?\|--\|--\|--\|--\|--\|[\s\S]*?(\n## |$)"
         new_content = re.sub(table_pattern, new_table + r"\1", old_content)
 
         if new_content != old_content:
