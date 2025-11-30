231934b7668230154ab8d4b2871a1ae172e91167
avencores <64781822+AvenCores@users.noreply.github.com>
2025-11-30 19:23:44 +0300

    Add new URL to EXTRA_URLS_FOR_26 list

--- source/main.py
+++ source/main.py
@@ -101,6 +101,7 @@ EXTRA_URLS_FOR_26 = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Cable.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
     "https://raw.githubusercontent.com/zieng2/wl/main/vless.txt",
+    "https://raw.githubusercontent.com/zieng2/wl/refs/heads/main/vless_universal.txt",
     "https://jsnegsukavsos.hb.ru-msk.vkcloud-storage.ru/love",
 ]
 
